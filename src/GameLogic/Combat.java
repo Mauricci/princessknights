b963c4c9f751bffc27c10cba0bc510f2133dc337
Jimmy Edberg <oathmountain@hotmail.com>
2018-09-03 13:52:58 +0200

    combat logic

--- src/GameLogic/Combat.java
+++ src/GameLogic/Combat.java
@@ -1,4 +1,21 @@
 package GameLogic;
 
+import java.util.Random;
+
 public class Combat {
+    public CombatResult calculateCombatResult(CombatVariables playerVariables, CombatVariables monsterVariables){
+        double combatStrength = playerVariables.getPower() / monsterVariables.getPower();
+        double chanceToWin = 50.0;
+        if(combatStrength < 1){
+            chanceToWin -= 10*(1-combatStrength);
+        }else if(combatStrength>1){
+            chanceToWin += 10*combatStrength;
+        }
+
+        Random random = new Random();
+
+        double result = chanceToWin*100 - random.nextInt(100);
+
+        return new CombatResult();//result);
+    }
 }
---
+++ src/GameLogic/CombatVariables.java
@@ -0,0 +1,7 @@
+package GameLogic;
+
+public class CombatVariables {
+    public double getPower(){
+        return 0.0;
+    }
+}
