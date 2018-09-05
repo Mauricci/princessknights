a735ef233503dde2fd36a1b645f48df7abd743d3
Jimmy Edberg <oathmountain@hotmail.com>
2018-09-05 17:07:20 +0200

    Done Scenario and gameloop

--- src/Dialog/Scenarios.java
+++
@@ -1,4 +0,0 @@
-package Dialog;
-
-public class Scenarios {
-}
--- src/Story/Scenario.java
+++ src/Story/Scenario.java
@@ -2,27 +2,29 @@ package Story;
 
 import Characters.Princess;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Scenario {
     //collection av scener
-    private Map<Integer, Scene> scenarioMap = new HashMap<>();
+    private Map<Integer, Scene> scenes;
     private SceneData currentSceneData;
 
-    public Scenario(List<Integer> scenesID){
-        //ladda in alla scener i en map
-
+    public Scenario(Map<Integer, Scene> scenes, int firstID){
+        this.scenes = scenes;
+        currentSceneData = new SceneData(null,firstID,StoryConstants.AUTONEXTQUESTION);
     }
-    public void doScenario(Princess princess){
+    public boolean doScenario(Princess princess){
         while(currentSceneData.getFlag() != StoryConstants.SCENARIODONE && currentSceneData.getFlag() != StoryConstants.COMBATDONE ) {
-            currentSceneData = scenarioMap.get(currentSceneData.getId()).doScene(princess);
+            currentSceneData = scenes.get(currentSceneData.getId()).doScene(princess);
             if(currentSceneData.getFlag() == StoryConstants.COMBATDONE){
                 //do some stuff
                 System.out.println("Nu har vi gjort en combat!");
             }
         }
+        return false;
 
     }
 }
