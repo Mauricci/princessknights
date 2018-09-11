c417833aa5681622c5b09badc4455d3d11d9c5f2
FonieFones <christophermauricci@gmail.com>
2018-09-11 18:14:44 +0200

    Fixed minor bugs in main method

--- src/PrincessKnights.java
+++ src/PrincessKnights.java
@@ -4,6 +4,8 @@ public class PrincessKnights {
 
     public static void main(String[] args) {
         Window window = new Window(null);
+        Game game = new Game(args[0], window);
+        boolean running = true;
         while(running){
             game.StartGame();
         }
